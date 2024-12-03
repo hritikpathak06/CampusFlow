5d29e4edf3bdefcf79f83a9317dbd097f9237c3a
Ritik Kumar Pathak <phritik06@gmail.com>
2024-12-03 19:44:40 +0530

    student design ui created

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,10 +1,26 @@
+import Announcements from '@/components/dashboard/Announcements'
+import EventCalendar from '@/components/dashboard/EventCalendar'
+import { BigCalendar } from '@/components/students/BigCalendar'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      <h1>Student Page</h1>
+    <>
+    <div className='flex p-4 gap-4 flex-col md:flex-row'>
+      {/* Left */}
+      <div className=" w-full lg:w-2/3 h-max  flex flex-col gap-8">
+     <div className="w-full bg-white p-4 rounded-md">
+      <h1 className=' text-xl font-bold'>Schedule 4A</h1>
+      <BigCalendar/>
+     </div>
+      </div>
+      {/* Right */}
+      <div className=" w-full lg:w-1/3 h-max  flex flex-col gap-8">
+          <EventCalendar />
+          <Announcements />
+        </div>
     </div>
+    </>
   )
 }
 
---
+++ src/components/students/BigCalendar.tsx
@@ -0,0 +1,55 @@
+"use client";
+import { Calendar, momentLocalizer, View, Views } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { useState } from "react";
+
+const localizer = momentLocalizer(moment);
+
+const myEventsList = [
+  {
+    title: "Meeting with Team",
+    start: new Date(2024, 11, 3, 10, 0),
+    end: new Date(2024, 11, 3, 12, 0),
+    description: "Discuss project milestones.",
+  },
+  {
+    title: "Lunch Break",
+    start: new Date(2024, 11, 3, 13, 0), // December 3, 2024, 1:00 PM
+    end: new Date(2024, 11, 3, 14, 0), // December 3, 2024, 2:00 PM
+    description: "Enjoy a relaxing lunch.",
+  },
+  {
+    title: "Client Presentation",
+    start: new Date(2024, 11, 4, 15, 0), // December 4, 2024, 3:00 PM
+    end: new Date(2024, 11, 4, 16, 30), // December 4, 2024, 4:30 PM
+    description: "Present the updated design to the client.",
+  },
+];
+
+export const BigCalendar = () => {
+  const [view, setView] = useState<View>(Views.WORK_WEEK);
+
+  const handleOnChangeView = (selectedView: View) => {
+    setView(selectedView);
+  };
+
+  return (
+    <>
+      <div>
+        <Calendar
+          localizer={localizer}
+          events={myEventsList}
+          startAccessor="start"
+          endAccessor="end"
+          views={["work_week", "day"]}
+          view={view}
+          onView={handleOnChangeView}
+          style={{ height: "98%" }}
+          min={new Date(2025,1,0,8,0,0)}
+          max={new Date(2025,1,0,18,0,0)}
+        />
+      </div>
+    </>
+  );
+};
